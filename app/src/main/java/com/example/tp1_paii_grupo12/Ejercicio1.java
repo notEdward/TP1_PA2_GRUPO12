6151905f236f3bf1d8205b806cbbbb2b99b6bf4d
Lisandro Jiménez <lisandro.25.lj@gmail.com>
2024-08-22 18:51:08 -0300

    creacion del activity para el ejercicio 1

--- app/src/main/java/com/example/tp1_paii_grupo12/Ejercicio1.java
+++ app/src/main/java/com/example/tp1_paii_grupo12/Ejercicio1.java
@@ -1,8 +1,6 @@
 package com.example.tp1_paii_grupo12;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,23 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Ejercicio1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_ejercicio1);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-    
-    public void Ejercicio1(View view ){
-        Intent ejercicio1 = new Intent(this, Ejercicio1.class);
-        startActivity(ejercicio1);
-    }
-    
+
+
 }
