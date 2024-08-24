aff60028aaaeffba21cb330a21912c88e40e9896
Lisandro Jiménez <lisandro.25.lj@gmail.com>
2024-08-24 13:44:26 -0300

    funcionalidad del ejercicio 1

--- app/src/main/java/com/example/tp1_paii_grupo12/Ejercicio1.java
+++ app/src/main/java/com/example/tp1_paii_grupo12/Ejercicio1.java
@@ -1,6 +1,9 @@
 package com.example.tp1_paii_grupo12;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Ejercicio1 extends AppCompatActivity {
 
+    private EditText et1;
+    private EditText et2;
+    private TextView result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ejercicio1);
+
+        et1 = (EditText)findViewById(R.id.txt_primerNumero);
+        et2 = (EditText)findViewById(R.id.txt_segundoNumero);
+        result = (TextView)findViewById(R.id.txt_resultado);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -22,5 +34,13 @@ public class Ejercicio1 extends AppCompatActivity {
         });
     }
 
+    public void Sumar(View view){
+        int num1 = Integer.parseInt(et1.getText().toString());
+        int num2 = Integer.parseInt(et2.getText().toString());
 
+        int suma = num1 + num2;
+        String resultado = String.valueOf(suma);
+
+        result.setText(resultado);
+    }
 }
